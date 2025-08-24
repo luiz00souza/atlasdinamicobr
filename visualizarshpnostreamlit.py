9f64df3e02fbe9d23f83bf67b450038cd0b0460a
luiz00souza <106404356+luiz00souza@users.noreply.github.com>
2025-08-24 20:30:08 -0300

    Update visualizarshpnostreamlit.py

--- visualizarshpnostreamlit.py
+++ visualizarshpnostreamlit.py
@@ -11,7 +11,9 @@ df_teste = pd.read_csv("ALLDATA_onehot_clipped.csv")
 
 # modulo_diretorio = r'C:\Users\campo\Desktop\mestrado\DADOS121223\Raster Reclassificados\88_rotinas_python'
 #import CHAMAMODELO
-shapefiles_folder_subdivididos = r"C:\Users\campo\Desktop\mestrado\DADOS121223\Raster Reclassificados\arrays padronizados\Shapefiles_Classes"
+#shapefiles_folder_subdivididos = r"C:\Users\campo\Desktop\mestrado\DADOS121223\Raster Reclassificados\arrays padronizados\Shapefiles_Classes"
+shapefiles_folder_subdivididos = "Shapefiles_Classes"
+
 os.makedirs(shapefiles_folder_subdivididos, exist_ok=True)
 
 #sys.path.append(modulo_diretorio)
@@ -412,6 +414,7 @@ def load_shapefiles(shapefiles_folder):
     return shapefiles
 
 # shapefiles_mesclados = load_shapefiles(shapefiles_folder_mesclados)
+
 
 
 
