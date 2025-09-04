b25e70a2b0023d306d02f464cf57a8be482a0b38
luiz00souza <106404356+luiz00souza@users.noreply.github.com>
2025-09-04 09:45:01 -0300

    Update WFS.py

--- WFS.py
+++ WFS.py
@@ -1,20 +1,21 @@
-import streamlit as st
-import geopandas as gpd
-import requests
-import geojson
 from owslib.wfs import WebFeatureService
+import geopandas as gpd
+import json
 
-# 1. Captura dos dados WFS
-url = 'https://ide.geobases.es.gov.br/geoserver/ows'
-wfs = WebFeatureService(url, version='1.1.0')
-layer = 'NOME_DA_CAMADA_AQUI'
-response = wfs.getfeature(typename=layer, outputFormat='application/json')
-gdf = gpd.GeoDataFrame.from_features(geojson.loads(response.read()))
+url = "https://ide.geobases.es.gov.br/geoserver/ows"
+wfs = WebFeatureService(url, version="1.1.0")
 
-# 2. Converter para colunas lat/lon
-gdf['latitude'] = gdf.geometry.y
-gdf['longitude'] = gdf.geometry.x
+# Veja todas as camadas disponíveis
+print(list(wfs.contents))
 
-# 3. Exibir mapa simples
-st.title("Visualização de WFS com Streamlit")
-st.map(gdf[['latitude', 'longitude']])
+# Escolha uma camada real do servidor
+layer = "geobases:limite_municipal"  # exemplo, troque pelo nome certo
+
+# Testar formatos alternativos
+response = wfs.getfeature(
+    typename=layer,
+    outputFormat="json"   # tente também "GEOJSON" ou "application/json"
+)
+
+gdf = gpd.GeoDataFrame.from_features(json.loads(response.read()))
+print(gdf.head())
