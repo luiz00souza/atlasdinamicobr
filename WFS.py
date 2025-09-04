08e90870aca2461ccdcfb68a3b08ef6f0f16da35
luiz00souza <106404356+luiz00souza@users.noreply.github.com>
2025-09-04 09:45:45 -0300

    Update WFS.py

--- WFS.py
+++ WFS.py
@@ -1,21 +1,8 @@
 from owslib.wfs import WebFeatureService
-import geopandas as gpd
-import json
 
 url = "https://ide.geobases.es.gov.br/geoserver/ows"
 wfs = WebFeatureService(url, version="1.1.0")
 
-# Veja todas as camadas disponíveis
-print(list(wfs.contents))
-
-# Escolha uma camada real do servidor
-layer = "geobases:limite_municipal"  # exemplo, troque pelo nome certo
-
-# Testar formatos alternativos
-response = wfs.getfeature(
-    typename=layer,
-    outputFormat="json"   # tente também "GEOJSON" ou "application/json"
-)
-
-gdf = gpd.GeoDataFrame.from_features(json.loads(response.read()))
-print(gdf.head())
+# Lista todas as camadas disponíveis
+for layer in list(wfs.contents):
+    print(layer)
